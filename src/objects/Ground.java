7f664984e91009fab2250915d74a1efc36d56488
Denys2002Holub <paloma20022001@gmail.com>
2020-03-31 00:53:58 +0200

    created class GroundImage

--- src/objects/Ground.java
+++ src/objects/Ground.java
@@ -1,4 +1,12 @@
 package objects;
 
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+
 public class Ground {
+
+    private class GroundImage {
+        BufferedImage groundImage;
+        int x;
+    }
 }
