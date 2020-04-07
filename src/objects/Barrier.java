a633f5e8a4c703d5a4318b786d19b499f05689ef
Denys2002Holub <paloma20022001@gmail.com>
2020-04-07 13:10:46 +0200

    added variables

--- src/objects/Barrier.java
+++ src/objects/Barrier.java
@@ -1,10 +1,12 @@
 package objects;
 
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 
 public class Barrier {
 
     public class BarrierImage {
         BufferedImage barrierImage;
+        int x,y;
     }
 }
