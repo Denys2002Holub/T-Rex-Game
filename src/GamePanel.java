b55e272aa3baef7ae399fe03e65afcb5cc145152
Denys2002Holub <paloma20022001@gmail.com>
2020-04-12 23:04:05 +0200

    changed variables \WIDTH and \HEIGHT into not-final, created constructor \GamePanel, added body into constructor \GamePanel

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -4,14 +4,15 @@ import objects.Ground;
 import objects.Sun;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class GamePanel extends JPanel implements Runnable, KeyListener {
 
-    public static final int
-            WIDTH = 800,
-            HEIGHT = 800;
+    public static int
+            WIDTH,
+            HEIGHT;
 
     private boolean
             running = false,
@@ -23,6 +24,22 @@ public class GamePanel extends JPanel implements Runnable, KeyListener {
     Sun sun;
 
     private int score;
+
+    public GamePanel() {
+        //WIDTH =
+        //HEIGHT =
+
+        ground = new Ground(HEIGHT);
+        dino = new Dino();
+        barrier = new Barrier(WIDTH);
+        sun = new Sun(HEIGHT, WIDTH);
+
+        score = 0;
+
+        setSize(WIDTH,HEIGHT);
+        setVisible(true);
+        setBackground(Color.lightGray);
+    }
 
     @Override
     public void keyTyped(KeyEvent e) {
