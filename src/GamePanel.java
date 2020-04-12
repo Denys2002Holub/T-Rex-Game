72dab7604ea90112f74c4cdf2f76a7db3922d9c9
Denys2002Holub <paloma20022001@gmail.com>
2020-04-12 23:21:17 +0200

    added possibility to play by pressing \space

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -87,7 +87,7 @@ public class GamePanel extends JPanel implements Runnable, KeyListener {
 
     @Override
     public void keyTyped(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_UP) {
+        if (e.getKeyCode() == KeyEvent.VK_UP || e.getKeyChar() == ' ') {
             if (gameOver) {
                 score = 0;
                 barrier.resume();
