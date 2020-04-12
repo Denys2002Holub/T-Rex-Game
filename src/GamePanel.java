6b8b5a65b460143c2486789a59c70182a26f60e5
Denys2002Holub <paloma20022001@gmail.com>
2020-04-12 22:55:50 +0200

    created all local variables:

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,12 +1,32 @@
+import objects.Barrier;
+import objects.Dino;
+import objects.Ground;
+import objects.Sun;
+
 import javax.swing.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class GamePanel extends JPanel implements Runnable, KeyListener {
 
+    public static final int
+            WIDTH = 800,
+            HEIGHT = 800;
+
+    private boolean
+            running = false,
+            gameOver = false;
+
+    Ground ground;
+    Dino dino;
+    Barrier barrier;
+    Sun sun;
+
+    private int score;
+
     @Override
     public void keyTyped(KeyEvent e) {
-        
+
     }
 
     @Override
