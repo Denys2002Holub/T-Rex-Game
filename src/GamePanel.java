6a8c29a1601b3eefc86301d576b42b34ad75f1a7
Denys2002Holub <paloma20022001@gmail.com>
2020-04-12 22:52:20 +0200

    implemented methods from interfaces

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,6 +1,26 @@
 import javax.swing.*;
+import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class GamePanel extends JPanel implements Runnable, KeyListener {
-    
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
