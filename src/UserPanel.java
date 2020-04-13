d6e65f85eeb2fd56f6a698b400edc008914d6485
Denys2002Holub <paloma20022001@gmail.com>
2020-04-13 02:20:02 +0200

    added body into \main

--- src/UserPanel.java
+++ src/UserPanel.java
@@ -28,6 +28,11 @@ public class UserPanel {
     }
 
     public static void main(String[] args) {
-        
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new UserPanel().createAndShowWindow();
+            }
+        });
     }
 }
