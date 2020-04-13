1bbf34a878e9721490d91f519306169348f7174e
Denys2002Holub <paloma20022001@gmail.com>
2020-04-13 02:12:51 +0200

    created new local variable \gameWindow

--- src/UserPanel.java
+++ src/UserPanel.java
@@ -1,9 +1,12 @@
+import javax.swing.*;
+
 public class UserPanel {
 
     public static int
             WIDTH = 800,
             HEIGHT = 800;
 
+    JFrame gameWindow = new JFrame("T-Rex game by raptor");
 
     public static void main(String[] args) {
 
