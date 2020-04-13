278bd8d1292363302333850faab9972915cf5386
Denys2002Holub <paloma20022001@gmail.com>
2020-04-13 02:20:41 +0200

    replaced \new Runnable with lambda

--- src/UserPanel.java
+++ src/UserPanel.java
@@ -28,11 +28,6 @@ public class UserPanel {
     }
 
     public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new UserPanel().createAndShowWindow();
-            }
-        });
+        javax.swing.SwingUtilities.invokeLater(() -> new UserPanel().createAndShowWindow());
     }
 }
